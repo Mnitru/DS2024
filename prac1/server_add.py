591c9f7b615a2f415d9c205869e44d58b21f1aa5
Mnitru <trungle1112003@gmail.com>
2024-11-27 14:33:30 +0700

    prac1

--- prac1/server_add.py
+++
@@ -1,9 +0,0 @@
-from xmlrpc.server import SimpleXMLRPCServer
-
-def add(a, b):
-    return a + b
-
-server = SimpleXMLRPCServer(("localhost", 8000))
-server.register_function(add, "add")
-print("Server running...")
-server.serve_forever()
